d501e55e62b526c82500bfa2e9e4c5df4b9dfcc2
fengxia41103 <feng_xia41103@hotmail.com>
2013-06-07 22:27:08 -0400

    Add common property to base handler.

--- anthem.py
+++ anthem.py
@@ -59,6 +59,15 @@ class MainPage(webapp2.RequestHandler):
 		self.response.write('hey feng')
 
 class MyBaseHandler(webapp2.RequestHandler):
+	def __init__(self, request=None, response=None):
+		webapp2.RequestHandler.__init__(self,request,response) # extend the base class
+		self.template_values={}
+		
+		self.template_values['me']=self.me=self.get_contact()
+		self.template_values['cart']=self.cart=self.get_open_cart()
+		self.template_values['url_login']=users.create_login_url(self.request.url)
+		self.template_values['url_logout']=users.create_logout_url('/')
+
 	def get_contact(self):
 		# manage contact -- who is using my service?
 		# update email and user name
@@ -84,9 +93,7 @@ class MyBaseHandler(webapp2.RequestHandler):
 		# 
 		# RULE -- single OPEN cart per terminal_seller rule
 		# open_cart=BuyOrderCart.query(BuyOrderCart.terminal_seller==me.key,BuyOrderCart.status=='Open')
-		me=self.get_contact()
-
-		open_cart=BuyOrderCart.query(ancestor=me.key).filter(BuyOrderCart.status=='Open')
+		open_cart=BuyOrderCart.query(ancestor=self.me.key).filter(BuyOrderCart.status=='Open')
 		assert open_cart.count()<2
 		if open_cart.count():
 			my_cart=open_cart.get() # if there is one
@@ -94,57 +101,45 @@ class MyBaseHandler(webapp2.RequestHandler):
 			# if no such cart, create one
 			# we are making this cart and Contact an entity group
 			# this will enforce data consistency
-			my_cart=BuyOrderCart(terminal_seller=me.key,status='Open',parent=me.key)
-			my_cart.owner=me.key
-			my_cart.last_modified_by=me.key
+			my_cart=BuyOrderCart(terminal_seller=self.me.key,status='Open',parent=self.me.key)
+			my_cart.owner=self.me.key
+			my_cart.last_modified_by=self.me.key
 			my_cart.shipping_cost=0
 			my_cart.put()
 		return my_cart
 							
 class EditBuyOrder(MyBaseHandler):
 	def get(self, order_id):
-		template_values = {}
-		template_values['me']=me=self.get_contact()
-		open_cart=template_values['cart']=self.get_open_cart()
-		
 		order=BuyOrder.get_by_id(int(order_id))
 		assert order
 		
 		# only owner of this order can edit
 		# TODO: super user should be able, too
-		if order.owner==me.key or me.can_be_super():
-			template_values['order']=order
+		if order.owner==self.me.key or self.me.can_be_super():
+			self.template_values['order']=order
 		else:
 			return self.response.write('You do not have the right to edit this order.')
 				
 		template = JINJA_ENVIRONMENT.get_template('/template/PublishNewBuyOrder.html')
-		self.response.write(template.render(template_values))
+		self.response.write(template.render(self.template_values))
 
 class PublishNewBuyOrder(MyBaseHandler):
 	def get(self):
-		me=self.get_contact()
-		if not me.can_be_doc():
-			template_values = {}
+		if not self.me.can_be_doc():
 			template = JINJA_ENVIRONMENT.get_template('/template/Membership_New.html')
-			self.response.write(template.render(template_values))
+			self.response.write(template.render(self.template_values))
 			return		
 
-		# load publish buyorder page
-		template_values = {}
-		template_values['me']=me
-		
 		# this is used to differentiate new order vs. editing an existing one
 		# since we are reusing this template HTML
-		template_values['order']=None
+		self.template_values['order']=None
 		
-		open_cart=template_values['cart']=self.get_open_cart()
 		template = JINJA_ENVIRONMENT.get_template('/template/PublishNewBuyOrder.html')
-		self.response.write(template.render(template_values))
+		self.response.write(template.render(self.template_values))
 
 	def post(self):
 		# Assumption: user hits GET first before POST
 		# so we don't need to check contact role anymore
-		me=self.get_contact()
 		
 		# create a new buyorder
 		client_email=self.request.POST['client'].strip()
@@ -172,8 +167,8 @@ class PublishNewBuyOrder(MyBaseHandler):
 				
 		# create a new buy order and add to store
 		order=BuyOrder()
-		order.owner=me.key
-		order.last_modified_by=me.key
+		order.owner=self.me.key
+		order.last_modified_by=self.me.key
 		if buyer: # buyer info is optional at post
 			order.terminal_buyer=buyer.key
 		order.name=product
@@ -186,29 +181,21 @@ class PublishNewBuyOrder(MyBaseHandler):
 
 class BrowseBuyOrderByOwnerByCat(MyBaseHandler):
 	def get(self,owner_id,cat):
-		me=self.get_contact()
-	
 		# load buyorder browse page
-		template_values = {}
-		template_values['url']=uri_for('buyorder-browse')		
-		template_values['me']=me
-		open_cart=template_values['cart']=self.get_open_cart()
-		template_values['url_login']=users.create_login_url(self.request.url)
-		template_values['url_logout']=users.create_logout_url('/')
+		self.template_values['url']=uri_for('buyorder-browse')		
 
 		# filter buyorder by owner
 		# and filter by category
 		queries=BuyOrder.query(ndb.AND(BuyOrder.owner==ndb.Key(Contact,owner_id), BuyOrder.queues==cat))
 		
 		# my open cart
-		open_cart=template_values['cart']=self.get_open_cart()
-		template_values['url_cart_review']=uri_for('cart-review')
+		self.template_values['url_cart_review']=uri_for('cart-review')
 		
-		if len(open_cart.fills):
+		if len(self.cart.fills):
 			# cart has some fills already, we will enforce
 			# a filter to display only BuyOrders from the same owner
 			# RULE -- unique (intermediate-buyer,terminal-seller) OPEN cart rule
-			owner_key = open_cart.fills[0].order.get().owner
+			owner_key = self.cart.fills[0].order.get().owner
 			queries=queries.filter(BuyOrder.owner==owner_key)
 				
 		# compose data structure for template
@@ -220,69 +207,53 @@ class BrowseBuyOrderByOwnerByCat(MyBaseHandler):
 			# place holder
 			d['filled by me']=0
 			data.append(d)		
-		template_values['buyorders']=data		
+		self.template_values['buyorders']=data		
 		
 		template = JINJA_ENVIRONMENT.get_template('/template/BrowseBuyOrder.html')
-		self.response.write(template.render(template_values))
+		self.response.write(template.render(self.template_values))
 
 class BrowseBuyOrderByOwner(MyBaseHandler):
 	def get(self,owner_id):
-		template_values = {}
-		template_values['me']=me=self.get_contact()
-		open_cart=template_values['cart']=self.get_open_cart()
-		
 		# limit to 10
 		orders=BuyOrder.query(BuyOrder.owner==ndb.Key(Contact,owner_id))
 				
 		# group them by "queues"
 		queue={}
 		for o in orders:
-			template_values['owner']=o.owner
+			self.template_values['owner']=o.owner
 			
 			# category as dict key
 			for q in o.queues:
 				if queue.has_key(q): queue[q].append(o)
 				else: queue[q]=[o]
 		
-		template_values['cats']=sorted(queue.keys())
-		template_values['orders']=queue
+		self.template_values['cats']=sorted(queue.keys())
+		self.template_values['orders']=queue
 		template = JINJA_ENVIRONMENT.get_template('/template/BrowseBuyOrderByOwner.html')
-		self.response.write(template.render(template_values))
+		self.response.write(template.render(self.template_values))
 		
 	def post(self,owner_id):
 		# return similar posts from the same owner
-		me=self.get_contact()
-		template_values = {}
 		queries=BuyOrder.query(BuyOrder.owner==ndb.Key(Contact,owner_id))
-		
 		self.response.write(json.dumps([json.dumps(o.to_dict(),cls=ComplexEncoder) for o in queries]))
 
 class BrowseBuyOrderById(MyBaseHandler):
 	def get(self,order_id):
-		template_values = {}
-		template_values['me']=me=self.get_contact()
-		template_values['cart']=me=self.get_open_cart()
-		template_values['order']=order=BuyOrder.get_by_id(int(order_id))
+		self.template_values['order']=order=BuyOrder.get_by_id(int(order_id))
 		assert order
 		
 		template = JINJA_ENVIRONMENT.get_template('/template/BrowseBuyOrderById.html')
-		self.response.write(template.render(template_values))
+		self.response.write(template.render(self.template_values))
 
 class BrowseBuyOrder(MyBaseHandler):
 	def get(self):
-		me=self.get_contact()
-		if not me.can_be_nur():
-			template_values = {}
+		if not self.me.can_be_nur():
 			template = JINJA_ENVIRONMENT.get_template('/template/Membership_New.html')
-			self.response.write(template.render(template_values))
+			self.response.write(template.render(self.template_values))
 			return		
 	
 		# load buyorder browse page
-		template_values = {}
-		template_values['url']=uri_for('buyorder-browse')		
-		template_values['me']=me
-		template_values['url_login']=users.create_login_url(self.request.url)
-		template_values['url_logout']=users.create_logout_url('/')
+		self.template_values['url']=uri_for('buyorder-browse')		
 
 		# filters
 		# filter by owner id
@@ -290,7 +261,7 @@ class BrowseBuyOrder(MyBaseHandler):
 			owner_id=self.request.GET['owner']
 		except:
 			owner_id=None
-		template_values['owner_id']=owner_id
+		self.template_values['owner_id']=owner_id
 			
 		# filter by Name or Description string match
 		try:
@@ -308,14 +279,13 @@ class BrowseBuyOrder(MyBaseHandler):
 			queries=BuyOrder.query()
 		
 		# my open cart
-		open_cart=template_values['cart']=self.get_open_cart()
-		template_values['url_cart_review']=uri_for('cart-review')
+		self.template_values['url_cart_review']=uri_for('cart-review')
 		
-		if len(open_cart.fills):
+		if len(self.cart.fills):
 			# cart has some fills already, we will enforce
 			# a filter to display only BuyOrders from the same owner
 			# RULE -- unique (intermediate-buyer,terminal-seller) OPEN cart rule
-			owner_key = open_cart.fills[0].order.get().owner
+			owner_key = self.cart.fills[0].order.get().owner
 			queries=queries.filter(BuyOrder.owner==owner_key)
 				
 		# compose data structure for template
@@ -327,15 +297,14 @@ class BrowseBuyOrder(MyBaseHandler):
 			# place holder
 			d['filled by me']=0
 			data.append(d)		
-		template_values['buyorders']=data		
+		self.template_values['buyorders']=data		
 		
 		template = JINJA_ENVIRONMENT.get_template('/template/BrowseBuyOrder.html')
-		self.response.write(template.render(template_values))
+		self.response.write(template.render(self.template_values))
 		
 	def post(self):
 		# Assumption: user has already hit GET before he can evern POST
 		# so we don't need to check Contact here
-		me=self.get_contact()
 		
 		# add a new buyorder fill to cart 
 		buyorder_id=self.request.POST['id']
@@ -346,14 +315,10 @@ class BrowseBuyOrder(MyBaseHandler):
 		buyorder=BuyOrder.get_by_id(int(buyorder_id))
 		assert buyorder
 		
-		# there is one and only one open cart		
-		my_cart=self.get_open_cart()
-		assert my_cart
-			
 		# we have established an OPEN cart
 		existing=False		
-		for i in xrange(len(my_cart.fills)):
-			f=my_cart.fills[i]
+		for i in xrange(len(self.cart.fills)):
+			f=self.cart.fills[i]
 			if f.order==buyorder.key:
 				# it's already in the cart, just update qty
 				existing=True
@@ -365,47 +330,42 @@ class BrowseBuyOrder(MyBaseHandler):
 			# if not existing, create a new fill and add to cart
 			# default client_price = price so you will break-even
 			f=BuyOrderFill(order=buyorder.key,price=price,qty=qty,client_price=price)
-			f.owner=me.key
-			f.last_modified_by=me.key
-			my_cart.fills.append(f)
-			my_cart.broker=buyorder.owner
+			f.owner=self.me.key
+			f.last_modified_by=self.me.key
+			self.cart.fills.append(f)
+			self.cart.broker=buyorder.owner
 			
 		# update cart
-		my_cart.put()
+		self.cart.put()
 		
-		self.response.write(json.dumps(my_cart.to_dict(),cls=ComplexEncoder))
+		self.response.write(json.dumps(self.cart.to_dict(),cls=ComplexEncoder))
 
 class ReviewCart(MyBaseHandler):
 	def get(self):
-		template_values = {}
 		cart_id=int(self.request.GET['cart'])
 		
-		me=self.get_contact()
-		cart=BuyOrderCart.get_by_id(cart_id,parent=me.key)
+		cart=BuyOrderCart.get_by_id(cart_id,parent=self.me.key)
 		assert cart
 
-		template_values['shipping_methods']=SHIPPING_METHOD
-		template_values['me']=me		
-		template_values['cart']=cart
-		template_values['url']=uri_for('cart-review')
+		self.template_values['shipping_methods']=SHIPPING_METHOD
+		self.template_values['cart']=cart
+		self.template_values['url']=uri_for('cart-review')
 		
 		# to save label file using BlobStore
-		template_values['shipping_form_url']=upload_url=blobstore.create_upload_url('/cart/shipping/%s' % (cart.key.id()))
+		self.template_values['shipping_form_url']=upload_url=blobstore.create_upload_url('/cart/shipping/%s' % (cart.key.id()))
 		
 		# serve label file if any
 		if cart.shipping_label:
-			template_values['shipping_label']='/blob/serve/%s/'% cart.shipping_label
+			self.template_values['shipping_label']='/blob/serve/%s/'% cart.shipping_label
 		else:
-			template_values['shipping_label']=None
+			self.template_values['shipping_label']=None
 		
 		template = JINJA_ENVIRONMENT.get_template('/template/ReviewCart.html')
-		self.response.write(template.render(template_values))
+		self.response.write(template.render(self.template_values))
 	
 	def post(self):
-		template_values = {}
 		cart_id=int(self.request.POST['cart'])
-		me=self.get_contact()
-		cart=BuyOrderCart.get_by_id(cart_id,parent=me.key)
+		cart=BuyOrderCart.get_by_id(cart_id,parent=self.me.key)
 		assert cart
 		status='0'
 		
@@ -463,37 +423,32 @@ class ReviewCart(MyBaseHandler):
 
 class BankingCart(MyBaseHandler):
 	def get(self):
-		template_values = {}
-		me=self.get_contact()
-		template_values['me']=me		
-		template_values['url']=uri_for('cart-banking')
-		template_values['review_url']=uri_for('cart-review')		
+		self.template_values['url']=uri_for('cart-banking')
+		self.template_values['review_url']=uri_for('cart-review')		
 		
-		carts=BuyOrderCart(parent=me.key).query()
+		carts=BuyOrderCart(parent=self.me.key).query()
 		
 		# payable carts
 		payable_carts=carts.filter(BuyOrderCart.payable_balance>0)
 		if self.request.GET.has_key('seller'):
 			seller_id=self.request.GET['seller']
 			payable_carts=payable_carts.filter(BuyOrderCart.terminal_seller==ndb.Key('Contact',seller_id))
-		template_values['payable_carts']=payable_carts
-		template_values['sellers']=set([c.terminal_seller for c in payable_carts])
+		self.template_values['payable_carts']=payable_carts
+		self.template_values['sellers']=set([c.terminal_seller for c in payable_carts])
 		
 		# receivable carts
 		receivable_carts=carts.filter(BuyOrderCart.receivable_balance>0)
 		if self.request.GET.has_key('client'):
 			client_id=self.request.GET['client']
 			receivable_carts=receivable_carts.filter(BuyOrderCart.terminal_buyer==ndb.Key('Contact',client_id))				
-		template_values['receivable_carts']=receivable_carts
-		template_values['clients']=set([c.terminal_buyer for c in receivable_carts if c.terminal_buyer])
+		self.template_values['receivable_carts']=receivable_carts
+		self.template_values['clients']=set([c.terminal_buyer for c in receivable_carts if c.terminal_buyer])
 		
 		# render
 		template = JINJA_ENVIRONMENT.get_template('/template/BankingCart.html')
-		self.response.write(template.render(template_values))
+		self.response.write(template.render(self.template_values))
 	
 	def post(self):
-		template_values = {}
-		me=self.get_contact()
 		status='0'
 		
 		action=self.request.POST['action']
@@ -503,25 +458,25 @@ class BankingCart(MyBaseHandler):
 		
 		if action=='payable':
 			for d in data:
-				cart=BuyOrderCart.get_by_id(int(d['id']),parent=me.key)
+				cart=BuyOrderCart.get_by_id(int(d['id']),parent=self.me.key)
 				assert cart
 				slip=AccountingSlip()
 				slip.amount=float(d['amount'])
-				slip.party_a=me.key
+				slip.party_a=self.me.key
 				slip.party_b=cart.terminal_seller
 				slip.money_flow='a-2-b'
-				slip.last_modified_by=me.key
+				slip.last_modified_by=self.me.key
 				payable_bundle.append(slip)
 		elif action=='receivable':
 			for d in data:
-				cart=BuyOrderCart.get_by_id(int(d['id']),parent=me.key)
+				cart=BuyOrderCart.get_by_id(int(d['id']),parent=self.me.key)
 				assert cart
 				slip=AccountingSlip()
 				slip.amount=float(d['amount'])
-				slip.party_a=me.key
+				slip.party_a=self.me.key
 				slip.party_b=cart.terminal_buyer
 				slip.money_flow='b-2-a'
-				slip.last_modified_by=me.key
+				slip.last_modified_by=self.me.key
 				receivable_bundle.append(slip)
 		
 		# write slips to data store		
@@ -539,58 +494,43 @@ class BankingCart(MyBaseHandler):
 
 class ManageUserProfile(MyBaseHandler):
 	def get(self):
-		template_values = {}
-		template_values['me']=me=self.get_contact()
-		template_values['membership_options']=MONTHLY_MEMBERSHIP_FEE
+		self.template_values['membership_options']=MONTHLY_MEMBERSHIP_FEE
 			
 		# render
 		template = JINJA_ENVIRONMENT.get_template('/template/ManageUserProfile.html')
-		self.response.write(template.render(template_values))
+		self.response.write(template.render(self.template_values))
 	
-	def post(self):
-		me=self.get_contact()
-		
 class ManageBuyOrderCart(MyBaseHandler):
 	def get(self):
-		template_values = {}
-		template_values['me']=me=self.get_contact()
-		template_values['review_url']=uri_for('cart-review')		
-		open_cart=template_values['cart']=self.get_open_cart()
+		self.template_values['review_url']=uri_for('cart-review')		
 		
 		# get all carts that belong to this user
-		template_values['my_carts']=carts=BuyOrderCart.query(ancestor=me.key).filter(BuyOrderCart.status!='Open')
+		self.template_values['my_carts']=carts=BuyOrderCart.query(ancestor=self.me.key).filter(BuyOrderCart.status!='Open')
 
 		# render
 		template = JINJA_ENVIRONMENT.get_template('/template/ManageBuyOrderCart.html')
-		self.response.write(template.render(template_values))
+		self.response.write(template.render(self.template_values))
 	
-	def post(self):
-		me=self.get_contact()
-
+		
 class ManageBuyOrder(MyBaseHandler):
 	def get(self):
-		template_values = {}
-		template_values['me']=me=self.get_contact()
-		
 		# get all carts that belong to this user
-		template_values['carts']=carts=BuyOrderCart.query(ancestor=me.key)
-		template_values['review_url']=uri_for('cart-review')		
-		template_values['browse_url']=uri_for('buyorder-browse')		
+		self.template_values['carts']=carts=BuyOrderCart.query(ancestor=self.me.key)
+		self.template_values['review_url']=uri_for('cart-review')		
+		self.template_values['browse_url']=uri_for('buyorder-browse')		
 		
 		# get new BuyOrders that are available to me		
-		if me.can_be_nur():
+		if self.me.can_be_nur():
 			orders=BuyOrder.query().fetch(100)
-		if me.can_be_doc():
+		if self.me.can_be_doc():
 			orders+=BuyOrder.query(BuyOrder.owner==me.key)
-		template_values['orders']=orders
+		self.template_values['orders']=orders
 		
 		# render
 		template = JINJA_ENVIRONMENT.get_template('/template/ManageBuyOrder.html')
-		self.response.write(template.render(template_values))
+		self.response.write(template.render(self.template_values))
 	
-	def post(self):
-		me=self.get_contact()
-		
+				
 class ShippingCart(blobstore_handlers.BlobstoreUploadHandler):
 	def post(self, cart_id):
 		user = users.get_current_user()
