a6bbc6367ff9ea11f44ef217ea1be85155350ba1
fengxia41103 <feng_xia41103@hotmail.com>
2013-08-29 13:10:28 -0400

    A bug in manageContact handler where try-except was commented out

--- anthem.py
+++ anthem.py
@@ -827,12 +827,13 @@ class ManageUserMembershipCancel(MyUserBaseHandler):
 
 class ManageUserContact(MyBaseHandler):
 	def get(self):	
-		#try:
-		if self.me.can_be_super():
-			self.template_values['me']=Contact.get_by_id(self.request.get('id'))
-			logging.debug('person identity swap')
-		else: pass
-		#except: pass
+		try:
+			if self.me.can_be_super():
+				self.template_values['me']=Contact.get_by_id(self.request.get('id'))
+				logging.debug('person identity swap')
+			else: pass
+		except: pass
+		
 		# render
 		template = JINJA_ENVIRONMENT.get_template('/template/ManageUserContact.html')
 		self.response.write(template.render(self.template_values))
